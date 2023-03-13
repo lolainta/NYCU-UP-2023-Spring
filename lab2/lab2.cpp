cf38133235e87b3a43820705af42e155c902094c
lolainta <lolainta@gmail.com>
2023-03-13 14:53:21 +0800

    style: remove unused code

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -4,18 +4,15 @@
 #include<filesystem>
 
 using namespace std;
-
-using str=string;
 namespace fs=std::filesystem;
 
 int main(int argc,char**argv){
-    str path(argv[1]),magic(argv[2]),content;
+    string path(argv[1]),magic(argv[2]),content;
     for(const auto&entry:fs::recursive_directory_iterator(path)){
         ifstream ifs(entry.path());
         ifs>>content;
         if(content==magic)
             return cout<<str(entry.path())<<endl,0;
     }
-    cout<<"Nothing"<<endl;
     return 0;
 }
