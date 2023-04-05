8a29df5173f18d74cab4a81c5ec39083a747cb3f
lolainta <lolainta@gmail.com>
2023-04-05 12:48:54 +0800

    feat: solver_asm loop index in stack

--- lab4/solver_asm.s
+++ lab4/solver_asm.s
@@ -7,25 +7,21 @@
     mov    rax,0x0a786c25203a
     mov    QWORD PTR [rbp-0x30],rax
 
-    mov    rax,0x18
+    mov    QWORD PTR [rbp-0x18],0x18
 LOOP:
-    mov    QWORD PTR [rbp-0x18],rax
-
     lea    rdi,QWORD PTR [rbp-0x30]
     lea    rsi,QWORD PTR [rbp-0x20]
-    add    rsi,rax
+    add    rsi,QWORD PTR [rbp-0x18]
     mov    rsi,QWORD PTR [rsi]
 
-    cmp    rax,0x28
+    cmp    QWORD PTR [rbp-0x18],0x28
     jne    NRA
     add    rsi,0xab
 NRA:
     mov    rdx,QWORD PTR [rbp-0x28]
     call   rdx
-
-    mov    rax,QWORD PTR [rbp-0x18]
-    add    rax,0x08
-    cmp    rax,0x30
+    add    QWORD PTR [rbp-0x18],0x08
+    cmp    QWORD PTR [rbp-0x18],0x30
     jne    LOOP
 
     leave
