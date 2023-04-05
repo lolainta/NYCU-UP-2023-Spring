f1d17838cb69f5059ea182537c7b165c6e4ff9ed
lolainta <lolainta@gmail.com>
2023-04-05 12:29:46 +0800

    feat: solver_asm with loop

--- lab4/solver_asm.s
+++ lab4/solver_asm.s
@@ -4,31 +4,29 @@
     mov    QWORD PTR [rbp-0x28],rdi
     mov    rax,QWORD PTR fs:0x28
     mov    QWORD PTR [rbp-0x8],rax
-    xor    eax,eax
     mov    rax,0x0a786c25203a
     mov    QWORD PTR [rbp-0x30],rax
 
-    lea    rdi,QWORD PTR [rbp-0x30]
-    lea    rsi,QWORD PTR [rbp-0x20]
-    add    rsi,0x18
-    mov    rsi,QWORD PTR [rsi]
-    mov    rdx,QWORD PTR [rbp-0x28]
-    call rdx
+    mov    rax,0x18
+LOOP:
+    mov    QWORD PTR [rbp-0x18],rax
 
     lea    rdi,QWORD PTR [rbp-0x30]
     lea    rsi,QWORD PTR [rbp-0x20]
-    add    rsi,0x20
+    add    rsi,rax
     mov    rsi,QWORD PTR [rsi]
-    mov    rdx,QWORD PTR [rbp-0x28]
-    call rdx
 
-    lea    rdi,QWORD PTR [rbp-0x30]
-    lea    rsi,QWORD PTR [rbp-0x20]
-    add    rsi,0x28
-    mov    rsi,QWORD PTR [rsi]
+    cmp    rax,0x28
+    jne    NRA
     add    rsi,0xab
+NRA:
     mov    rdx,QWORD PTR [rbp-0x28]
-    call rdx
+    call   rdx
+
+    mov    rax,QWORD PTR [rbp-0x18]
+    add    rax,0x08
+    cmp    rax,0x30
+    jne    LOOP
 
     leave
     ret
