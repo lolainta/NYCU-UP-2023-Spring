b0f1bd333ad0e916335dc5e4811c7c878bd596ef
lolainta <lolainta@gmail.com>
2023-05-06 15:50:31 +0800

    feat: quick sort interative

--- lab6/qsort.c
+++ lab6/qsort.c
@@ -1,13 +1,20 @@
 void qsort(long arr[],int n){
-    quickSort(arr,0,n-1);
+    int stack[64]={0,n-1};
+    int top=1;
+    while(top>=0){
+        int h=stack[top--];
+        int l=stack[top--];
+        int p=partition(arr,l,h);
+        if(p-1>l){
+            stack[++top]=l;
+            stack[++top]=p-1;
+        }
+        if(h>p+1){
+            stack[++top]=p+1;
+            stack[++top]=h;
+        }
+    }
 }
-/*
-void swap(long*a,long*b){
-  long t=*a;
-  *a=*b;
-  *b=t;
-}
-*/
 
 #define SWAP(a,b) do{ \
     long temp=a; \
@@ -21,26 +28,10 @@ inline int partition(long array[],int low,int high){
     int i=(low-1);
     for(int j=low;j<high;j++){
         if(array[j]<=pivot){
-            i++;
+            ++i;
             SWAP(array[i],array[j]);
         }
     }
     SWAP(array[i+1],array[high]);
     return i+1;
-}
-
-void quickSort(long array[],int low,int high){ 
-    if(high-low<=8){
-        for(int i=low;i<high;++i){
-            for(int j=i+1;j<high;++j){
-                if(array[i]>array[j])
-                    SWAP(array[i],array[j]);
-            }
-        }
-    }
-    if(low<high){
-        int pi=partition(array,low,high);
-        quickSort(array,low,pi-1);
-        quickSort(array,pi+1,high);
-    }
 }
--- lab6/qsort.s
+++ lab6/qsort.s
@@ -1,107 +1,139 @@
 	.p2align 4
 qsort:
-	lea	edx, -1[rsi]
+	push	r14
+	mov	r10, rdi
+	mov	ecx, 32
 	xor	eax, eax
-	xor	esi, esi
-	jmp	quickSort
-quickSort:
-	movsx	rax, edx
 	push	r13
-	mov	r8d, esi
+	sub	esi, 1
 	push	r12
-	lea	r13, [rdi+rax*8]
+	mov	r12d, 1
 	push	rbp
-	mov	rbp, rax
+	mov	ebp, 1
 	push	rbx
-	mov	eax, ebp
-	mov	rbx, rdi
-	sub	eax, r8d
-	sub	rsp, 8
-	cmp	eax, 10
-	jle	.L21
-.L2:
-	cmp	ebp, r8d
-	jle	.L1
-.L19:
-	movsx	r10, r8d
-.L7:
-	mov	edx, ebp
-	mov	rdi, QWORD PTR 0[r13]
-	lea	r12d, -1[r8]
-	lea	rax, [rbx+r10*8]
-	sub	edx, r8d
-	add	rdx, r10
-	lea	rsi, [rbx+rdx*8]
+	sub	rsp, 136
+	lea	rdi, -120[rsp]
+	rep stosq
+	mov	DWORD PTR -116[rsp], esi
 	.p2align 4,,10
 	.p2align 3
-.L12:
+.L16:
+	lea	r13d, -1[rbp]
+.L10:
+	mov	ebx, DWORD PTR -120[rsp+r12*4]
+	movsx	rax, r13d
+	mov	r11d, DWORD PTR -120[rsp+rax*4]
+	movsx	rax, ebx
+	lea	r14, [r10+rax*8]
+	lea	ecx, -1[r11]
+	mov	rdi, QWORD PTR [r14]
+	cmp	ebx, r11d
+	jle	.L3
+	mov	edx, ebx
+	movsx	rsi, r11d
+	sub	edx, r11d
+	lea	rax, [r10+rsi*8]
+	add	rdx, rsi
+	lea	r8, [r10+rdx*8]
+	.p2align 4,,10
+	.p2align 3
+.L5:
 	mov	rdx, QWORD PTR [rax]
 	cmp	rdi, rdx
-	jl	.L11
-	add	r12d, 1
-	movsx	rcx, r12d
-	lea	rcx, [rbx+rcx*8]
-	mov	r9, QWORD PTR [rcx]
-	mov	QWORD PTR [rcx], rdx
+	jl	.L4
+	add	ecx, 1
+	movsx	rsi, ecx
+	lea	rsi, [r10+rsi*8]
+	mov	r9, QWORD PTR [rsi]
+	mov	QWORD PTR [rsi], rdx
 	mov	QWORD PTR [rax], r9
-.L11:
+.L4:
 	add	rax, 8
-	cmp	rax, rsi
-	jne	.L12
-	movsx	rax, r12d
-	mov	rcx, QWORD PTR 0[r13]
-	mov	esi, r8d
-	mov	rdi, rbx
-	lea	rax, 8[rbx+rax*8]
+	cmp	r8, rax
+	jne	.L5
+	mov	rdi, QWORD PTR [r14]
+.L3:
+	movsx	rax, ecx
+	lea	rax, 8[r10+rax*8]
 	mov	rdx, QWORD PTR [rax]
-	mov	QWORD PTR [rax], rcx
-	mov	QWORD PTR 0[r13], rdx
-	mov	edx, r12d
-	call	quickSort
-	lea	r8d, 2[r12]
-	mov	eax, ebp
-	sub	eax, r8d
-	cmp	eax, 10
-	jg	.L2
-.L21:
-	cmp	ebp, r8d
-	jle	.L1
-	lea	edi, 1[r8]
-	cmp	ebp, edi
-	jle	.L19
-	movsx	r10, r8d
-	movsx	r9, edi
-	lea	rsi, [rbx+r10*8]
+	mov	QWORD PTR [rax], rdi
+	mov	QWORD PTR [r14], rdx
+	cmp	r11d, ecx
+	jl	.L6
+	add	ecx, 2
+	sub	ebp, 2
+	cmp	ecx, ebx
+	jl	.L7
+	test	ebp, ebp
+	js	.L14
+	movsx	r12, ebp
+	jmp	.L16
 	.p2align 4,,10
 	.p2align 3
 .L6:
-	mov	rax, r9
+	mov	DWORD PTR -120[rsp+r12*4], ecx
+	add	ecx, 2
+	cmp	ebx, ecx
+	jle	.L10
+.L7:
+	lea	eax, 1[rbp]
+	add	ebp, 2
+	cdqe
+	movsx	r12, ebp
+	mov	DWORD PTR -120[rsp+rax*4], ecx
+	mov	DWORD PTR -120[rsp+r12*4], ebx
+	jmp	.L16
 	.p2align 4,,10
 	.p2align 3
-.L9:
-	mov	rdx, QWORD PTR [rsi]
-	mov	rcx, QWORD PTR [rbx+rax*8]
-	cmp	rdx, rcx
-	jle	.L8
-	mov	QWORD PTR [rsi], rcx
-	mov	QWORD PTR [rbx+rax*8], rdx
-.L8:
-	add	rax, 1
-	cmp	ebp, eax
-	jg	.L9
-	add	edi, 1
-	add	r9, 1
-	add	rsi, 8
-	cmp	ebp, edi
-	jne	.L6
-	jmp	.L7
-	.p2align 4,,10
-	.p2align 3
-.L1:
-	add	rsp, 8
+.L14:
+	add	rsp, 136
 	pop	rbx
 	pop	rbp
 	pop	r12
 	pop	r13
+	pop	r14
 	ret
 	.p2align 4
+partition:
+	movsx	rax, edx
+	mov	r9, rdi
+	lea	r11, [rdi+rax*8]
+	lea	edi, -1[rsi]
+	mov	r8, QWORD PTR [r11]
+	cmp	edx, esi
+	jle	.L22
+	movsx	rax, esi
+	sub	edx, esi
+	add	rdx, rax
+	lea	rcx, [r9+rax*8]
+	lea	rsi, [r9+rdx*8]
+	.p2align 4,,10
+	.p2align 3
+.L21:
+	mov	rax, QWORD PTR [rcx]
+	cmp	rax, r8
+	jg	.L20
+	add	edi, 1
+	movsx	rdx, edi
+	lea	rdx, [r9+rdx*8]
+	mov	r10, QWORD PTR [rdx]
+	mov	QWORD PTR [rdx], rax
+	mov	QWORD PTR [rcx], r10
+.L20:
+	add	rcx, 8
+	cmp	rsi, rcx
+	jne	.L21
+	mov	r8, QWORD PTR [r11]
+	lea	eax, 1[rdi]
+.L19:
+	movsx	rdi, edi
+	lea	rdx, 8[r9+rdi*8]
+	mov	rcx, QWORD PTR [rdx]
+	mov	QWORD PTR [rdx], r8
+	mov	QWORD PTR [r11], rcx
+	ret
+	.p2align 4,,10
+	.p2align 3
+.L22:
+	mov	eax, esi
+	jmp	.L19
